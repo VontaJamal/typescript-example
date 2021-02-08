04e3fc118fd920cbbea3c823ac41913023d1ada0
Vonta Johnson <devontajohnsonit@gmail.com>
2021-02-08 10:45:29 -0500

    setup routing for team page and players page

--- src/components/nbaTeamList.tsx
+++ src/components/nbaTeamList.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import {Team} from '../interfaces/interfaces'
-import {Image} from '@chakra-ui/react'
+import {Image, LinkBox} from '@chakra-ui/react'
+import {Link} from '@reach/router'
 
 import {MotionBox} from './motionComponents/motionComponents'
 
@@ -13,24 +14,27 @@ const NBATeam = ({
 }) => {
   const {full_name: name, image} = team
   return (
-    <MotionBox
-      alignSelf='center'
-      align='center'
-      _hover={{cursor: 'pointer'}}
-      p={['.3rem', 5]}
-      onMouseEnter={() => setTeam(team.name)}
-      onMouseLeave={() => setTeam('')}
-    >
-      <Image
-        boxSize={['lg', '15rem']}
-        w={[null, null, 'auto']}
-        h={['auto', null]}
-        // h='auto'
-        objectFit='contain'
-        src={image}
-        alt={`${name} Logo`}
-      />
-    </MotionBox>
+    <LinkBox>
+      <Link to='/players'>
+        <MotionBox
+          alignSelf='center'
+          align='center'
+          _hover={{cursor: 'pointer'}}
+          p={['.3rem', 5]}
+          onMouseEnter={() => setTeam(team.name)}
+          onMouseLeave={() => setTeam('')}
+        >
+          <Image
+            boxSize={['lg', '15rem']}
+            w={[null, null, 'auto']}
+            h={['auto', null]}
+            objectFit='contain'
+            src={image}
+            alt={`${name} Logo`}
+          />
+        </MotionBox>
+      </Link>
+    </LinkBox>
   )
 }
 
